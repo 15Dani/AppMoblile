2b04ea9fe0dae02312305f2dbf46207df2753ba7
Danielia Oliveira <danielia.de@bol.com.br>
2020-09-30 17:25:15 -0300

    finalizando telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './Components/Home/Home.component';
+import { LoginComponent } from './Components/Login/Login.component';
+import { AtualizarComponent } from './Components/Pedidos/atualizar/atualizar.component';
+import { SenhaComponent } from './Components/Senha/Senha.component';
+import { CriarComponent } from './Components/Pedidos/criar/criar.component';
+import { DeletaComponent } from './Components/Pedidos/deleta/deleta.component';
+import { DetalhesitensComponent } from './Components/Pedidos/detalhesitens/detalhesitens.component';
+import { ListaComponent } from './Components/Pedidos/lista/lista.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AtualizarComponent,
+    CriarComponent,
+    DeletaComponent,
+    DetalhesitensComponent,
+    ListaComponent,
+    SenhaComponent
+
   ],
   imports: [
     BrowserModule,
